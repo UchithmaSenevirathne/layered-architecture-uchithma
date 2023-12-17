7f419cf3b48fd21f5bcc9d527cab3c365080c376
UchithmaSenevirathne <uchithma@gmail.com>
2023-12-18 01:07:29 +0530

    Content Updated

--- src/main/java/com/example/layeredarchitecture/dao/impl/OrderDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/impl/OrderDAOImpl.java
@@ -4,6 +4,7 @@ import com.example.layeredarchitecture.dao.OrderDAO;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.CustomerDTO;
 import com.example.layeredarchitecture.model.OrderDTO;
+import com.example.layeredarchitecture.util.SQLUtil;
 
 import java.sql.*;
 import java.time.LocalDate;
@@ -12,34 +13,23 @@ import java.util.ArrayList;
 public class OrderDAOImpl implements OrderDAO {
     @Override
     public String generateNewOrderId() throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        Statement stm = connection.createStatement();
-        ResultSet rst = stm.executeQuery("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
+        ResultSet rst = SQLUtil.execute("SELECT oid FROM `Orders` ORDER BY oid DESC LIMIT 1;");
 
         return rst.next() ? String.format("OID-%03d", (Integer.parseInt(rst.getString("oid").replace("OID-", "")) + 1)) : "OID-001";
     }
 
     @Override
     public boolean searchOrderId(String orderId) throws SQLException, ClassNotFoundException {
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("SELECT oid FROM `Orders` WHERE oid=?");
-        stm.setString(1, orderId);
-
-        System.out.println("if exist oid..."+stm.executeQuery().next());
-
-        return stm.executeQuery().next();
+        ResultSet resultSet = SQLUtil.execute("SELECT oid FROM `Orders` WHERE oid=?", orderId);
+        return resultSet.next();
     }
 
     @Override
     public boolean saveOrder(OrderDTO orderDTO) throws SQLException, ClassNotFoundException {
-        System.out.println(orderDTO);
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)");
-        stm.setString(1, orderDTO.getOrderId());
-        stm.setDate(2, Date.valueOf(orderDTO.getOrderDate()));
-        stm.setString(3, orderDTO.getCustomerId());
-
-        return stm.executeUpdate() > 0;
+        return SQLUtil.execute("INSERT INTO `Orders` (oid, date, customerID) VALUES (?,?,?)",
+                orderDTO.getOrderId(),
+                Date.valueOf(orderDTO.getOrderDate()),
+                orderDTO.getCustomerId());
     }
 
 }
