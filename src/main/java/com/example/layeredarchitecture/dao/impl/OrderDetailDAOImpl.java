3fe827b5e15eb5294e7b3c192840a63bfca47ccc
UchithmaSenevirathne <uchithma@gmail.com>
2023-12-18 01:09:07 +0530

    Content Updated

--- src/main/java/com/example/layeredarchitecture/dao/impl/OrderDetailDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/impl/OrderDetailDAOImpl.java
@@ -3,6 +3,7 @@ package com.example.layeredarchitecture.dao.impl;
 import com.example.layeredarchitecture.dao.OrderDetailDAO;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.OrderDetailDTO;
+import com.example.layeredarchitecture.util.SQLUtil;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,14 +15,10 @@ public class OrderDetailDAOImpl implements OrderDetailDAO {
 
     @Override
     public boolean saveOrderDetails(OrderDetailDTO orderDetailDTO) throws SQLException, ClassNotFoundException {
-        System.out.println(orderDetailDTO);
-        Connection connection = DBConnection.getDbConnection().getConnection();
-        PreparedStatement stm = connection.prepareStatement("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)");
-        stm.setString(1, orderDetailDTO.getOrderId());
-        stm.setString(2, orderDetailDTO.getItemCode());
-        stm.setBigDecimal(3, orderDetailDTO.getUnitPrice());
-        stm.setInt(4, orderDetailDTO.getQty());
-
-        return stm.executeUpdate() > 0;
+        return SQLUtil.execute("INSERT INTO OrderDetails (oid, itemCode, unitPrice, qty) VALUES (?,?,?,?)",
+                orderDetailDTO.getOrderId(),
+                orderDetailDTO.getItemCode(),
+                orderDetailDTO.getUnitPrice(),
+                orderDetailDTO.getQty());
     }
 }
