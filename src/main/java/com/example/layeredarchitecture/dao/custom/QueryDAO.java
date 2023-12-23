865967a968afe97f06e42b3fc8ce1e09b9b89c91
UchithmaSenevirathne <uchithma@gmail.com>
2023-12-23 11:56:47 +0530

    content updated

--- src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/QueryDAO.java
@@ -1,11 +1,12 @@
 package com.example.layeredarchitecture.dao.custom;
 
+import com.example.layeredarchitecture.dao.SuperDAO;
 import com.example.layeredarchitecture.model.CustomDTO;
 import com.example.layeredarchitecture.model.CustomerDTO;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public interface QueryDAO {
+public interface QueryDAO extends SuperDAO {
     ArrayList<CustomDTO> customerOrderDetails(String search) throws SQLException, ClassNotFoundException;
 }
